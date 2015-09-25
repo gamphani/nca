63c9a4da02c4586e23ef392f01f544b81005052b
Dave G. Phiri <davegamphaniphiri@gmail.com>
2015-09-25 09:29:12 +0200

    clean up migrations

--- db/migrate/20150921080539_add_fields_to_form_health_reproductive_health_output_threes.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToFormHealthReproductiveHealthOutputThrees < ActiveRecord::Migration
-  def change
-    add_column :form_health_reproductive_health_output_threes, :start_date, :date
-    add_column :form_health_reproductive_health_output_threes, :end_date, :date
-    add_column :form_health_reproductive_health_output_threes, :created_at, :date
-    add_column :form_health_reproductive_health_output_threes, :updated_at, :date
-  end
-end
--- db/migrate/20150921080653_remove_fields_to_form_health_reproductive_health_output_threes.rb
+++
@@ -1,7 +0,0 @@
-class RemoveFieldsToFormHealthReproductiveHealthOutputThrees < ActiveRecord::Migration
-  def change
-    remove_column :form_health_reproductive_health_output_threes, :start_date, :string
-    remove_column :form_health_reproductive_health_output_threes, :end_date, :string
-    remove_column :form_health_reproductive_health_output_threes, :created_at, :string
-  end
-end
